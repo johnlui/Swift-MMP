9d8b8de6b62269b68d38aee50fadefd6001cbf77
JohnLui <wenhanlv@gmail.com>
2017-09-11 00:14:00 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Swift-MMP
+------
+
+Swift 编写的 Material Design 风格的流媒体音乐播放器，简称 MMP，用于 iOS 系统。
+
+
+### 开发中，敬请期待
+
+
+## 开源协议
+
+本项目遵循 MIT 协议开源，具体请查看根目录下的 LICENSE 文件。
---
+++ Swift-MMP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-MMP
+//
+//  Created by John Lui on 2017/9/11.
+//  Copyright © 2017年 John Lui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
