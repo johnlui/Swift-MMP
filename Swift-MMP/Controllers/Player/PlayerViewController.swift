7d8fa773fb6d1029abd200ad5c5b4fcb80a8d8f9
JohnLui <wenhanlv@gmail.com>
2017-09-13 20:39:29 +0800

    player page has perfect UI now

--- Swift-MMP/Controllers/Player/PlayerViewController.swift
+++ Swift-MMP/Controllers/Player/PlayerViewController.swift
@@ -14,6 +14,7 @@ class PlayerViewController: UIViewController {
 
     @IBOutlet weak var backView: UIView!
     @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var albumImageView: UIImageView!
     @IBOutlet weak var songNameLabel: UILabel!
     @IBOutlet weak var albumLabel: UILabel!
     @IBOutlet weak var statedTimeLabel: UILabel!
@@ -22,7 +23,10 @@ class PlayerViewController: UIViewController {
         super.viewDidLoad()
 
         let urlString = Common.thumbBaseURI + song[1] + ".jpg"
-        self.backgroundImageView.kf.setImage(with: URL(string: urlString.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!))
+        if let string = urlString.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) {
+            self.backgroundImageView.kf.setImage(with: URL(string: string))
+            self.albumImageView.kf.setImage(with: URL(string: string))
+        }
         
         self.songNameLabel.text = song[0]
         self.albumLabel.text = song[1]
@@ -35,6 +39,14 @@ class PlayerViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 0.2, delay: 0, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+            self.albumImageView.alpha = 1
+        }, completion: nil)
     }
     
 
@@ -51,4 +63,32 @@ class PlayerViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
 
+    @IBAction func randomButtonBeTapped(_ sender: Any) {
+        let imagesArray = [#imageLiteral(resourceName: "ic_list_repeat"), #imageLiteral(resourceName: "ic_one_shot"), #imageLiteral(resourceName: "ic_shuffle_white_36dp")]
+        if let button = sender as? UIButton,
+            let currentImage = button.currentImage,
+            let index = imagesArray.index(of: currentImage) {
+            var i = index + 1;
+            if i > 2 {
+                i = 0
+            }
+            button.setImage(imagesArray[i], for: UIControlState.normal)
+        }
+    }
+    @IBAction func prevButtonBeTapped(_ sender: Any) {
+    }
+    @IBAction func pauseButtonBeTapped(_ sender: Any) {
+        if let button = sender as? UIButton {
+            
+        }
+    }
+    @IBAction func nextButtonBeTapped(_ sender: Any) {
+    }
+    @IBAction func heartButtonBeTapped(_ sender: Any) {
+        if let button = sender as? UIButton {
+            let image = button.currentImage == #imageLiteral(resourceName: "ic_favorite_white_48dp") ? #imageLiteral(resourceName: "ic_favorite_red_48dp") : #imageLiteral(resourceName: "ic_favorite_white_48dp")
+            button.setImage(image, for: UIControlState.normal)
+        }
+    }
+    
 }
